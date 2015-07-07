b883e192947daef2b25c1becf99b03c36936873f
Astrid Paris <astrid@astridparis.com>
2015-07-07 15:33:32 -0400

    Starting campaign page's goal donut

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+
+
+// Doughnut chart to display goal progress on campaign pages
+var percentGoal = [
+  {
+    value: 33,
+    color:"#4eddc1"
+  },
+  {
+    value : 66,
+    color : "#E2EAE9"
+  }
+]
+var options = {
+  percentageInnerCutout : 75
+};
+var goalPie= $("#percentGoal").get(0).getContext("2d");
+var myWeeklyGoal = new Chart(goalPie).Doughnut(percentGoal, options);
