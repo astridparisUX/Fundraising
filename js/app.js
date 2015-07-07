8d826ff9de87b428021e56000c7197d749dd3752
Astrid Paris <astrid@astridparis.com>
2015-07-07 15:47:55 -0400

    Correcting wrong paths

--- js/app.js
+++ js/app.js
@@ -5,18 +5,27 @@ $(document).foundation();
 
 
 // Doughnut chart to display goal progress on campaign pages
-var percentGoal = [
+
+
+var percentages = [
   {
     value: 33,
     color:"#4eddc1"
   },
   {
-    value : 66,
-    color : "#E2EAE9"
+    value: 66,
+    color: "#E2EAE9"
   }
 ]
 var options = {
-  percentageInnerCutout : 75
+  percentageInnerCutout: 75
 };
-var goalPie= $("#percentGoal").get(0).getContext("2d");
-var campaignDonut = new Chart(goalPie).Doughnut(percentGoal, options);
+
+// "percentGoal" below: defines a variable that is targeting an element 
+// with an id of "percentGoal", which is in campaign.html
+var percentGoal= $("#percentGoal").get(0).getContext("2d");
+
+// "campaignDonut" below: defines a new variable that creates 
+// the new chart for the previous variable "percentGoal" and 
+// brings in the different values and defines the options we’ve changed
+var campaignDonut = new Chart(percentGoal).Doughnut(percentages, options);
